50c35e0a44c609bcc50bc0407ac77eb8e578aa55
Goodoose <ko.chernyshenko@gmail.com>
2019-02-25 17:25:48 +0200

    game with mouse was added

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -5,7 +5,7 @@
   const cells = [];
   const cellQuantity = 16;
 
-  const createCells = function createCells() {
+  function createCells() {
     for (let i = 0; i < cellQuantity; i++) {
       const newCell = {
         id: i,
@@ -18,54 +18,76 @@
       }
       cells.push(newCell);
     }
-  };
-  const swap = function swap(item1, item2) {
+  }
+
+  function swap(item1, item2) {
     const temp = cells[item1];
     cells[item1] = cells[item2];
     cells[item2] = temp;
-  };
+  }
 
-  const renderCells = function renderCell() {
+  function renderCells() {
     container.innerHTML = '';
     for (let i = 0; i < cells.length; i++) {
-      const cellPazzle = document.createElement('div');
-      cellPazzle.textContent = cells[i].name;
-      cellPazzle.classList = 'cell__container';
-      container.appendChild(cellPazzle);
+      const cellPuzzle = document.createElement('div');
+      cellPuzzle.textContent = cells[i].name;
+      cellPuzzle.classList = 'cell__container';
+      cellPuzzle.dataset.cellNum = cells[i].name;
+      container.appendChild(cellPuzzle);
     }
-  };
+  }
 
-  const shaflCells = function shafl() {
+  function shaflCells() {
     for (let i = 0; i < cellQuantity; i++) {
       const item1 = Math.round(Math.random() * (cellQuantity - 1));
       const item2 = Math.round(Math.random() * (cellQuantity - 1));
       swap(item1, item2);
     }
     renderCells();
-  };
+  }
 
-  const isEmptyCell = function isEmptyCell() {
+  function findCell(num) {
     for (let i = 0; i < cells.length; i++) {
-      if (cells[i].empty === true) {
+      if (cells[i].name === num) {
         return i;
       }
     }
-  };
+    return 0;
+  }
 
-  const winnerCombination = function vinnerCombination() {
+  function winnerCombination() {
     for (let i = 0; i < cellQuantity - 1; i++) {
       if (i !== cells[i].id) {
         return;
       }
     }
-    alert('You Winner!!!');
-  };
+    alert('You Won!!!');
+  }
 
   createCells();
   renderCells();
+  shaflCells();
+
+  container.addEventListener('click', (e) => {
+    const targetNum = +e.target.closest('.cell__container').dataset.cellNum;
+    const tempNum = findCell('');
+    const clickedNumber = findCell(targetNum);
+
+    const revUp = clickedNumber + 1 !== 4 && clickedNumber + 1 !== 8 && clickedNumber + 1 !== 12;
+    const revDown = clickedNumber - 1 !== 3 && clickedNumber - 1 !== 7 && clickedNumber - 1 !== 11;
+
+    if (clickedNumber + 4 === tempNum
+      || clickedNumber - 4 === tempNum
+      || (clickedNumber + 1 === tempNum && revUp)
+      || (clickedNumber - 1 === tempNum && revDown)) {
+      swap(clickedNumber, tempNum);
+    }
+    renderCells();
+    setTimeout(winnerCombination, 0);
+  });
 
   document.addEventListener('keydown', (e) => {
-    const tempNum = isEmptyCell();
+    const tempNum = findCell('');
     if (e.keyCode === 39) {
       if (tempNum > 0 && tempNum - 1 !== 3 && tempNum - 1 !== 7 && tempNum - 1 !== 11) {
         swap(tempNum - 1, tempNum);
@@ -99,4 +121,5 @@
   buttonShafl.addEventListener('click', () => {
     shaflCells();
   });
+
 })();
